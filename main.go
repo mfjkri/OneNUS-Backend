119bafb9acf1b80705a53f8bb794f56ab4061efa
mfjkri <muhammadfikri6844@gmail.com>
2022-12-19 16:11:15 +0800

    Whitelisted new frontend subdomain

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func CORSConfig() cors.Config {
     corsConfig.AllowOrigins = []string{
 		"http://localhost:3000",
 		"http://192.168.0.100:3000",
-		"https://www.onenus.link",
+		"https://app.onenus.link",
 	}
     corsConfig.AllowCredentials = true
     corsConfig.AddAllowHeaders("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers", "Content-Type", "X-XSRF-TOKEN", "Accept", "Origin", "X-Requested-With", "Authorization")
