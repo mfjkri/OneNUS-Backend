a006c05c62e72b4325b55f0dbb9f172f3d89c253
mfjkri <muhammadfikri6844@gmail.com>
2022-12-26 11:13:34 +0800

    feat: ping request now returns app version number

--- routes/public.go
+++ routes/public.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mfjkri/OneNUS-Backend/controllers"
 )
@@ -13,7 +15,7 @@ func RegisterPublicRoutes(r *gin.Engine) {
 	// misc
 	r.GET("ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong 1",
+			"message": "oneNUS " + os.Getenv("APP_VERSION"),
 		})
 	})
 }
