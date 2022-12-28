0861a967ca58ffd89e9c206fc99a4b8dcb9340c1
mfjkri <muhammadfikri6844@gmail.com>
2022-12-28 20:03:01 +0800

    fix: include postsCount and commentsCount in response

--- controllers/auth.go
+++ controllers/auth.go
@@ -60,16 +60,20 @@ func VerifyAuth(c *gin.Context) (user models.User, found bool) {
 
 // Convert a User Model into a JSON format
 type UserResponse struct {
-	ID       uint   `json:"id" binding:"required"`
-	Username string `json:"username" binding:"required"`
-	Role     string `json:"role" binding:"required"`
+	ID            uint   `json:"id" binding:"required"`
+	Username      string `json:"username" binding:"required"`
+	Role          string `json:"role" binding:"required"`
+	PostsCount    uint   `json:"postsCount" binding:"required"`
+	CommentsCount uint   `json:"commentsCount" binding:"required"`
 }
 
 func CreateUserResponse(user *models.User) UserResponse {
 	return UserResponse{
-		ID:       user.ID,
-		Username: user.Username,
-		Role:     user.Role,
+		ID:            user.ID,
+		Username:      user.Username,
+		Role:          user.Role,
+		PostsCount:    user.PostsCount,
+		CommentsCount: user.CommentsCount,
 	}
 }
 
